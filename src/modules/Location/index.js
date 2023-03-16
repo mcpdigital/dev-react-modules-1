465e8c19718dd68304a8417052277e0e073ee5e7
mcpdigital <internet@mcpdigital.com>
2023-03-16 02:44:58 -0300

    fff

---
+++ src/modules/Location/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Games = () => (
+    <div>Location Module</div>
+);
+
+export default {
+    routeProps: {
+        path: '/location',
+        component: Games,
+    },
+    name: 'Location',
+};
